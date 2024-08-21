155c5d1314eed063dd7ec40ebd5b78c1b2ad56fa
Pratik limbu <123137412+limbupratik@users.noreply.github.com>
2024-08-21 14:26:30 +0545

    change

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import hashlib
 
-
 def convert_to_md5(word):
     word_bytes = word.encode('utf-16')
     result = hashlib.md5(word_bytes)
@@ -12,13 +11,18 @@ with open('input.txt', 'r') as file:
 
 words = content.split()
 
-
 hashed_words = [convert_to_md5(word) for word in words]
 
 
 with open('hash_data.txt', 'w') as output_file:
     for word, hashed_word in zip(words, hashed_words):
-        output_file.write(f'{hashed_word}\n')
+        output_file.write(f'{word}->{hashed_word}\n')
 
-print("Data is converted to sha256 'hash_data.txt'.")
+print("Data is converted to MD5 'hash_data.txt'.")
+
+with open('hash_data_sha256.txt', 'w') as output_file:
+    for word, hashed_word in zip(words, hashed_words):
+        output_file.write(f'{word}-> {hashed_word}\n')
+
+print("Hashed data has been written to 'hash_data_sha256.txt'.")
 
